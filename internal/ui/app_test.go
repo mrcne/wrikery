c26447ddd6c3e268b483d0b41026023d8aaf6a50
Marcin <242138+mrcne@users.noreply.github.com>
2026-09-05 07:30:06 +0200

    ui application shell with bubbletea

---
+++ internal/ui/app_test.go
@@ -0,0 +1,24 @@
+package ui_test
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/x/exp/teatest"
+
+	"github.com/mrcne/wrikery/internal/ui"
+)
+
+func TestAppShowsTitleAndQuitsOnQ(t *testing.T) {
+	tm := teatest.NewTestModel(t, ui.New("test"),
+		teatest.WithInitialTermSize(80, 24))
+
+	teatest.WaitFor(t, tm.Output(), func(b []byte) bool {
+		return strings.Contains(string(b), "wrike-tui test")
+	}, teatest.WithDuration(2*time.Second))
+
+	tm.Send(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("q")})
+	tm.WaitFinished(t, teatest.WithFinalTimeout(2*time.Second))
+}
